0bb5eb6c4937aea2b89f73f2807fdda3e7430c00
Lmedeiros-leiman <lmedeiros045@gmail.com>
2023-10-24 01:56:13 -0300

    reformulando arquivos

--- Node2D.gd
+++ Node2D.gd
@@ -40,6 +40,10 @@ var H_maxima = ProjectSettings.get("display/window/size/viewport_height") / 40 #
 var grid_size : Vector2i = Vector2i(W_maxima,H_maxima)
 var grid: Array
 
+var player_body_positions
+var player_size = 3
+var player_head_position = Vector2()
+var player_last_movement = Vector2()
 
 func restart_game() -> void:
 	grid = []
@@ -51,10 +55,21 @@ func restart_game() -> void:
 	
 	# 
 	# creates player on the center.
+	# math here is convoluted and loud.
+	# it works. but my senses want to "optimize" this.
+	#
+	player_size = 3
+	player_head_position = Vector2((grid.size()/2) ,(grid[grid.size()/2].size()/2))
+	player_body_positions = []
+	
+	# 0 will always be head.
+	player_body_positions.append(player_head_position)
+	player_body_positions.append(Vector2((grid.size()/2 -1) ,(grid[grid.size()/2].size()/2)))
+	player_body_positions.append(Vector2((grid.size()/2 -2) ,(grid[grid.size()/2].size()/2)))
+
 	grid[ (grid.size()/2)   ][ (grid[grid.size()/2].size()/2) ] = "snakefacerigth"
 	grid[grid.size()/2 -1][grid[grid.size()/2].size()/2] = "snakebodyhorizontal"
 	grid[grid.size()/2 -2][grid[grid.size()/2].size()/2] = "snakeendright"
-
 
 func add_apple() -> bool:
 
@@ -64,8 +79,6 @@ func add_apple() -> bool:
 		var x = randi() % gridCopy.size() -1
 		var y = randi() % gridCopy[x].size() -1
 		
-		var formatedString = "TENTANDO COLOCAR MAÇA EM (%s , %s )" % [x,y]
-		print(formatedString)
 		if gridCopy.size()-1 < 1:
 			gridCopy = null
 			return false
@@ -73,7 +86,6 @@ func add_apple() -> bool:
 
 
 		if (gridCopy[x][y] != null):
-				push_error("FRACASOU.")
 				# if the current space is already filled.
 				gridCopy[x].pop_at(y)
 				# if the entire row is filled.
@@ -81,7 +93,6 @@ func add_apple() -> bool:
 					gridCopy.pop_at(x)
 					#if end;
 		else:
-			print("SUCESSO.")
 			# if the current space is empty place our apple!
 			grid[x][y] = "apple"
 			gridCopy = null # removes array copy from memory
@@ -90,16 +101,11 @@ func add_apple() -> bool:
 			break # we have our apple!
 	
 	# if the loop ever breaks... by some god like interference.
-	push_error("ALGO DEU ERRADO. O LOOP ADD_APPLE QUEBROU.")
 	gridCopy = null
 	return false
 
-
-
 func redraw_arena() -> void:
-
 	#grid[x][y]
-
 
 	for i in (grid.size()):
 		for j in (grid[i].size()):
@@ -109,15 +115,54 @@ func redraw_arena() -> void:
 				Vector2(i,j),
 				0,
 				TILEMAP[grid[i][j]]
-
-
-
-		)
+			)
+		#end for j
+	#end for i
+	pass
 
 func move_player(movement) -> bool:
 
 
 	return true
+
+func get_player_movement() -> Vector2 :
+
+	
+	var y = -(int(Input.is_action_pressed("up")) - int(Input.is_action_pressed("down")))
+	var x = -(int(Input.is_action_pressed("left")) - int(Input.is_action_pressed("right")))
+
+	# to avoid problems, lets allow only 1 movement at a time.
+	if ( y != 0 and x != 0):
+		y = 0
+	
+	if ( y == 0 and x == 0):
+		return player_last_movement
+	
+	player_last_movement = Vector2(x ,y)
+
+	return player_last_movement
+	pass
+
+func validate_movement() -> bool:
+	# if this return false i guess the player must DIE?
+	# and if true just... move the player?
+	var current_movement = get_player_movement()
+	var x = current_movement.x
+	var y = current_movement.y
+
+	var old_player_head_position = player_head_position
+	player_head_position += current_movement
+	
+	grid[player_head_position.x][player_head_position.y] = grid[old_player_head_position.x][old_player_head_position.y]
+	grid[old_player_head_position.x][old_player_head_position.y] = null
+	
+
+	print( old_player_head_position)
+	print( player_head_position)
+
+
+	return true
+
 
 
 func _ready(): 
@@ -143,8 +188,10 @@ func _process(_delta):
 	current_time += _delta
 
 	if current_time > delay_duration:
-		restart_game()
+		# this will run every time the player gets an apple.
 		add_apple()
+		# but overal, just run validate movement.
+		validate_movement()
 
 		redraw_arena()
 		
@@ -153,6 +200,5 @@ func _process(_delta):
 	
 
 	pass
-
 
 
